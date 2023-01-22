8365f7022b00b882f5e5063d16f6ff0f5374e17d
Codekacode <karem.quiroz.m@gmail.com>
2023-01-22 00:16:23 -0500

    Add Cofetti and change button to new game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Die from './components/Die';
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   const [dices, setDices] = useState(allNewDice())
@@ -48,6 +49,7 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies Game</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
@@ -65,7 +67,7 @@ function App() {
       </div>
 
       <button onClick={rollDice} className="roll-dice">
-        Roll Dice
+        {tenzies? "New Game" : "Roll Game"}
       </button>
     </main>
   );
